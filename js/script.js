0fcdf296ecf703b5fa3e4635b08740bb004feed8
matteolpera <matteo.lepera02@gmail.com>
2025-11-06 15:22:58 +0100

    milestone 1 end

--- js/script.js
+++ js/script.js
@@ -22,47 +22,74 @@
 //////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 
-const chilometriDaPercorrere = document.getElementById("km");
-console.log(chilometriDaPercorrere);
+const chilometriInput = document.getElementById("km");
 const nomeUtente = document.getElementById("name");
-console.log(nomeUtente);
 const button = document.getElementById("btn");
-console.log(button)
-const etaUtente = document.getElementById("eta");
-console.log(etaUtente);
+const etaInput = document.getElementById("eta");
+const output = document.querySelector(".result");
+console.log(chilometriInput, nomeUtente, button, etaInput);
 
-button.addEventListener
+button.addEventListener("click", function () {
 
-// if (isNaN(chilometriDaPercorrere)) {
-//     alert("Errore: devi inserire i chilometri che vuoi percorrere!");
-// } else if (chilometriDaPercorrere < 1) {
-//     alert("I chilometri che hai inserito non sono validi!");
-// } else {
+    const chilometriDaPercorrere = parseInt(chilometriInput.value);
+    if (isNaN(chilometriDaPercorrere)) {
+        alert("Errore: devi inserire i chilometri che vuoi percorrere!");
+    } else if (chilometriDaPercorrere < 1) {
+        alert("I chilometri che hai inserito non sono validi!");
+    } else {
+
+        const etaUtente = parseInt(etaInput.value);
+
+        if (isNaN(etaUtente)) {
+            alert("Errore: devi inserire la tua età!");
+        } else if (etaUtente < 1) {
+            alert("La tua età non è valida!");
+        }
+
+        let tariffaBase = chilometriDaPercorrere * 0.21;
+        let prezzoFinale = "";
+
+        let scontoGiovani = tariffaBase * 0.20; //sconto 20%
+        let scontoAnziani = tariffaBase * 0.40; //sconto 40%
+
+        let prezzoScontato = tariffaBase;
+
+        if (etaUtente < 18) {
+            prezzoScontato = tariffaBase - scontoGiovani;
+
+        } else if (etaUtente >= 65) {
+            prezzoScontato = tariffaBase - scontoAnziani;
+
+        }
+
+        prezzoFinale = `Il prezzo del tuo biglietto è ${prezzoScontato = prezzoScontato.toFixed(2)}€`;
+
+        output.innerText = prezzoFinale;
+    }
+})
 
 
 
-// if (isNaN(etaUtente)) {
-//     alert("Errore: devi inserire la tua età!");
-// } else if (etaUtente < 1) {
-//     alert("La tua età non è valida!");
+
+
+
+
+// let tariffaBase = chilometriDaPercorrere * 0.21;
+
+
+// let scontoGiovani = tariffaBase * 0.20; //sconto 20%
+// let scontoAnziani = tariffaBase * 0.40; //sconto 40%
+
+// let prezzoScontato = tariffaBase;
+
+// if (etaUtente < 18) {
+//     prezzoScontato = tariffaBase - scontoGiovani;
+
+// } else if (etaUtente >= 65) {
+//     prezzoScontato = tariffaBase - scontoAnziani;
+
 // }
 
-let tariffaBase = chilometriDaPercorrere * 0.21;
-
-
-let scontoGiovani = tariffaBase * 0.20; //sconto 20%
-let scontoAnziani = tariffaBase * 0.40; //sconto 40%
-
-let prezzoScontato = tariffaBase;
-
-if (etaUtente < 18) {
-    prezzoScontato = tariffaBase - scontoGiovani;
-
-} else if (etaUtente >= 65) {
-    prezzoScontato = tariffaBase - scontoAnziani;
-
-}
-
-console.log(`Il prezzo del tuo biglietto è ${prezzoScontato = prezzoScontato.toFixed(2)}€`);
+// console.log(`Il prezzo del tuo biglietto è ${prezzoScontato = prezzoScontato.toFixed(2)}€`);
 
 // }
